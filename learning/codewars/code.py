9095c823bab5d2e6a1ed4142e55165bcac444374
Diego Menin <dmenin@gmail.com>
2016-04-08 19:30:29 +0100

    .

--- learning/codewars/code.py
+++ learning/codewars/code.py
@@ -11,3 +11,28 @@ def compose(f, g):
 compose(add1,this)(5)
 compose(add1,this)(3,1)
 ##########################################################
+
+#Diamonds
+
+def diamond(n):
+    if  n % 2 == 0:
+        return None
+    
+    #n = -1
+    S = ""
+    levels = (n - 1)/2
+    
+    for l in range(levels,0,-1):
+        stars = n-(l*2)    
+        S += l*" " + "*" * stars +"\n"
+    S +=  "*" * n + "\n"
+    for l in range(1,levels+1,1):
+        stars = n-(l*2)
+        S +=  l*" " + "*" * stars + "\n"
+    
+    return S
+
+print diamond(3)
+print diamond(13)
+
+##########################################################
